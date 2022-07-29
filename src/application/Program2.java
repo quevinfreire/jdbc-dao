abd9dcf132223c868d42b737600f3b5361bbcb0e
Quevin Freire <quevinfreire93@gmail.com>
2022-07-29 03:43:45 -0400

    DepartmentDaoJDBC (findById and findAll implementation)

---
+++ src/application/Program2.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findById =====");
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
+		
+
+		System.out.println("\n=== TEST 2: department findAll =====");
+		List<Department> list = departmentDao.findAll();
+		for (Department dep : list) {
+			System.out.println(dep);	
+		}
+		
+
+	}
+	
+	//TODO: Aprender a criar o findById do department.
+	//Erro na linha -> (DepartmentDaoJDBC.java:45)
+
+}
